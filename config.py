45f8b01999fe3ec44f40c2422587dd8b9d345e03
Michael <crazedmonkey231@users.noreply.github.com>
2024-08-12 00:25:05 -0400

    add type hints in config

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 import os
+from typing import AnyStr, LiteralString
 
 #
 # Game Config
 #
-DEBUG = False
+DEBUG: bool = False
 
 # Asset directories
-MAIN_DIR = os.path.split(os.path.abspath(__file__))[0]
-IMAGE_DIR = os.path.join(MAIN_DIR, 'images')
-AUDIO_DIR = os.path.join(MAIN_DIR, 'audio')
-GIF_DIR = os.path.join(MAIN_DIR, 'gif')
+MAIN_DIR: AnyStr = os.path.split(os.path.abspath(__file__))[0]
+IMAGE_DIR: LiteralString = os.path.join(MAIN_DIR, 'images')
+AUDIO_DIR: LiteralString = os.path.join(MAIN_DIR, 'audio')
+GIF_DIR: LiteralString = os.path.join(MAIN_DIR, 'gif')
 
 # fps
 FPS: int = 120
@@ -18,8 +19,8 @@ FPS: int = 120
 SCREEN_SIZE: tuple[int, int] = (1280, 720)
 SCREEN_WIDTH: int = SCREEN_SIZE[0]
 SCREEN_WIDTH_HALF: int = SCREEN_SIZE[0] // 2
-SCREEN_HEIGHT = SCREEN_SIZE[1]
-SCREEN_HEIGHT_HALF = SCREEN_SIZE[1] // 2
+SCREEN_HEIGHT: int = SCREEN_SIZE[1]
+SCREEN_HEIGHT_HALF: int = SCREEN_SIZE[1] // 2
 
 # Canvas
 CANVAS_SIZE: tuple[int, int] = (10000, 10000)
@@ -32,18 +33,18 @@ CANVAS_HEIGHT_HALF: int = CANVAS_SIZE[1] // 2
 GRID_WIDTH: int = 32
 GRID_SIZE: int = CANVAS_WIDTH // GRID_WIDTH
 CELL_SIZE: int = CANVAS_WIDTH // GRID_SIZE
-DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+DIRECTIONS: list[tuple[int, int]] = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
 # Colors RGB
-RGB_WHITE = (255, 255, 255)
-RGB_BLACK = (0, 0, 0)
-RGB_RED = (255, 0, 0)
-RGB_GREEN = (0, 255, 0)
-RGB_BLUE = (0, 0, 255)
+RGB_WHITE: tuple[int, int, int] = (255, 255, 255)
+RGB_BLACK: tuple[int, int, int] = (0, 0, 0)
+RGB_RED: tuple[int, int, int] = (255, 0, 0)
+RGB_GREEN: tuple[int, int, int] = (0, 255, 0)
+RGB_BLUE: tuple[int, int, int] = (0, 0, 255)
 
 # Colors RGBA
-RGBA_WHITE = (255, 255, 255, 255)
-RGBA_BLACK = (0, 0, 0, 255)
-RGBA_RED = (255, 0, 0, 255)
-RGBA_GREEN = (0, 255, 0, 255)
-RGBA_BLUE = (0, 0, 255, 255)
+RGBA_WHITE: tuple[int, int, int, int] = (255, 255, 255, 255)
+RGBA_BLACK: tuple[int, int, int, int] = (0, 0, 0, 255)
+RGBA_RED: tuple[int, int, int, int] = (255, 0, 0, 255)
+RGBA_GREEN: tuple[int, int, int, int] = (0, 255, 0, 255)
+RGBA_BLUE: tuple[int, int, int, int] = (0, 0, 255, 255)
