d7417e863a3ee30315a0263a4ae0ff65b23d8c02
Michael <crazedmonkey231@users.noreply.github.com>
2024-08-15 19:21:33 -0400

    added overlay on the screen

--- toolbox/camera.py
+++ toolbox/camera.py
@@ -15,6 +15,7 @@ class CameraRenderer(RenderUpdates):
         self.camera_lookat_pos = (SCREEN_WIDTH_HALF, SCREEN_HEIGHT_HALF)
         self.top_left = (self.camera_lookat_pos[0] - SCREEN_WIDTH_HALF, self.camera_lookat_pos[1] - SCREEN_HEIGHT_HALF)
         self.zoom_scale = 1
+        self.screen_overlay = None
 
     def mouse_pos_to_global_pos(self):
         mouse_pos = pygame.mouse.get_pos()
@@ -28,6 +29,8 @@ class CameraRenderer(RenderUpdates):
     def draw(self, canvas):
         if not self.screen:
             self.screen = pygame.display.get_surface()
+            self.screen_overlay = Surface((SCREEN_WIDTH, SCREEN_HEIGHT)).convert_alpha()
+            self.screen_overlay.fill((0, 0, 0, 0))
         surface_blit = canvas.blit
         dirty = self.lostsprites
         self.lostsprites = []
@@ -61,7 +64,8 @@ class CameraRenderer(RenderUpdates):
         snapshot.blit(canvas, (0, 0), snapshot_rect)
 
         # Scale snapshot to screen size and center then blit on to screen
-        _s, _r = toolbox.util.scale_image_smooth(snapshot, (SCREEN_WIDTH, SCREEN_HEIGHT),
-                                                 (SCREEN_WIDTH_HALF, SCREEN_HEIGHT_HALF))
-        self.screen.blit(_s, (0, 0), _r)
+        scaled_surface, scaled_rect = toolbox.util.scale_image_smooth(snapshot, (SCREEN_WIDTH, SCREEN_HEIGHT),
+                                                                      (SCREEN_WIDTH_HALF, SCREEN_HEIGHT_HALF))
+        self.screen.blit(scaled_surface, (0, 0), scaled_rect)
+        self.screen.blit(self.screen_overlay, (0, 0))
         return dirty
